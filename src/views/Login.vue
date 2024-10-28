8e13e12008d33fcbe0ed1212057b66b61f4c87ce
SKSolutions1992 <sokhy.khim@outlook.com>
2024-10-28 09:52:29 +0700

    login update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center h-screen px-6 bg-gray-200">
-    <div class="w-full max-w-sm p-6 bg-white rounded-md shadow-md">
+    <!-- <div class="w-full max-w-sm p-6 bg-white rounded-md shadow-md">
       <div class="flex items-center justify-center">
         
         <span class="text-2xl font-semibold text-gray-700  underline">Login E-Form </span>
@@ -20,8 +20,43 @@
           </button>
         </div>
       </form>
-      <!-- <button @click="getAllDepartmentAndSections()">Test</button> -->
+    </div> -->
+    <div class="flex items-center justify-center h-screen px-6 bg-gray-200">
+  <div class="w-full max-w-sm">
+    <div class="overflow-hidden bg-white border rounded-md shadow-md">
+      <form @submit.prevent="submitForm">
+        <div class="flex items-center justify-center bg-red-600 px-5 py-3 text-gray-700 border-b">
+          <h3 class="text-sm text-white">
+            LOGIN E-FORM
+          </h3>
+        </div>
+
+        <div class="px-5 py-6 text-gray-700 bg-gray-100 border-b">
+          <label class="text-xs"><span class="text-sm text-gray-700">លេខសម្គាល់បុគ្គលិក / <span>Employee ID</span></span></label>
+
+          <div class="relative mt-2 rounded-md shadow-sm">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
+              <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+</svg>
+            </span>
+
+            <input v-model="form.EmployeeID" type="text" placeholder="លេខកូដបុគ្គលិក/ID"
+                   class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
+          </div>
+        </div>
+
+        <div class="flex items-center justify-center bg-gray-100 px-5 py-3">
+          <button
+              class="w-full px-4 py-2 text-sm text-center text-black-700 bg-gray-300 rounded-md focus:outline-none hover:bg-green-600 hover:text-white">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
+  </div>
+</div>
   </div>
 </template>
 
