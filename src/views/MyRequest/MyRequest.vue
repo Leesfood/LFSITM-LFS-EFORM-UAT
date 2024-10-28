37b5a9843855e9927d7cd537d31f1cfeebc4c92c
SKSolutions1992 <sokhy.khim@outlook.com>
2024-10-28 10:25:01 +0700

    Update my request list

--- src/views/MyRequest/MyRequest.vue
+++ src/views/MyRequest/MyRequest.vue
@@ -1,5 +1,31 @@
 <template>
-  <h3 class="text-gray-700 text-3xl font-medium">
-    Coming Soon !
-  </h3>
+  <div class="flex items-center justify-center">
+    
+    <div class="w-full">
+      <div class="md:px-1 py-4 pl-4 md:py-7 bg-white rounded-tl-lg rounded-tr-lg">
+        <h3 class="text-red-700 text-3xl font-medium">under constraction!!!!</h3>
+      </div>
+      <!-- Responsive table -->
+      <div class="bg-white shadow px-4 pb-5 overflow-x-auto">
+        <table class="w-full table-auto border border-gray-200">
+          <thead>
+            <tr class="w-full text-sm leading-none text-gray-800 border-b border-gray-200 whitespace-nowrap">
+              <th class="font-normal py-2 text-center border-r border-gray-200">No</th>
+              <th class="font-normal py-4 px-4 text-left border-r border-gray-200 hidden md:table-cell">Request Type</th>
+              <th class="font-normal py-4 text-left px-4 border-r border-gray-200">From Date</th>
+              <th class="font-normal py-4 text-left px-4 border-r border-gray-200">To Date</th>
+              <th class="font-normal text-left px-4 border-r border-gray-200">Reason</th>
+              <th class="font-normal text-left px-4 border-r border-gray-200">Approver</th>
+              <th class="font-normal text-left px-4 border-r border-gray-200">Status</th>
+              <th class="font-normal text-left px-4 border-r border-gray-200">Remark</th>
+              <th class="font-normal border-r border-gray-200">Action</th>
+            </tr>
+          </thead>
+          
+        </table>
+      </div>
+      
+    </div>
+  </div>
 </template>
+
